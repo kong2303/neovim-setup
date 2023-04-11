c067b07d7b1f92a1c016a286f5beb1b43904fd62
mokipha <mgisun6119@gmail.com>
2023-04-12 02:16:53 +0900

    first commit

---
+++ lua/sunbean/set.lua
@@ -0,0 +1,16 @@
+vim.opt.nu = true
+vim.opt.linebreak = true
+vim.opt.showbreak = '+++'
+vim.opt.showmatch = true
+
+vim.opt.hlsearch = true
+vim.opt.smartcase = true
+vim.opt.ignorecase = true
+vim.opt.incsearch = true
+
+vim.opt.autoindent = true
+vim.opt.expandtab = true
+vim.opt.shiftwidth = 2
+vim.opt.smartindent = true
+vim.opt.smarttab = true
+vim.opt.softtabstop = 2
